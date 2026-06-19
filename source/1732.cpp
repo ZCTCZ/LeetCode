23bee7bc6be3d82f81b8582523732c65488dcbe9
zctcz <2932742577@qq.com>
2026-06-19 23:57:51 +0800

    LC1732.找到最高海拔

---
+++ source/1732.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+    int largestAltitude(std::vector<int>& gain)
+    {
+        int ans = 0;
+        int cur = 0;
+        for (auto e : gain) {
+            cur += e;
+            ans = std::max(ans, cur);
+        }
+
+        return ans;
+    }
+};
