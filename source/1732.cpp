86d66b6bf426002cab806260b45ca5089f304b40
zctcz <2932742577@qq.com>
2026-07-02 22:13:16 +0800

    格式修改

--- source/1732.cpp
+++ source/1732.cpp
@@ -2,16 +2,15 @@
 #include <vector>
 
 class Solution {
-public:
-    int largestAltitude(std::vector<int>& gain)
-    {
-        int ans = 0;
-        int cur = 0;
-        for (auto e : gain) {
-            cur += e;
-            ans = std::max(ans, cur);
-        }
-
-        return ans;
+ public:
+  int largestAltitude(std::vector<int>& gain) {
+    int ans = 0;
+    int cur = 0;
+    for (auto e : gain) {
+      cur += e;
+      ans = std::max(ans, cur);
     }
+
+    return ans;
+  }
 };
